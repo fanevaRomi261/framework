bbedec61da3bc3b0d20c932c9a0a7f06c59113db
fanevaRomi261 <118768031+fanevaRomi261@users.noreply.github.com>
2023-03-07 07:21:07 +0300

    sprint 1

---
+++ src/java/etu1866/framework/servlet/FrontServlet.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package etu1866.framework.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import utility.Util;
+
+/**
+ *
+ * @author faneva
+ */
+public class FrontServlet extends HttpServlet {
+
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response,String url)
+            throws ServletException, IOException {
+            PrintWriter out = response.getWriter();
+            /* TODO output your page here. You may use following sample code. */
+            String []valiny = Util.lien(url);
+            for(String v:valiny){
+                out.println("<p>"+v+"</p>");
+            }            
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String url = request.getRequestURL().toString();
+        processRequest(request, response,url);
+        RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/jsp/test.jsp");
+        disp.forward(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String url = request.getRequestURL().toString();
+        processRequest(request, response,url);
+    }
+
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
